06919ea2cb9ea89bf81f1837817aebf1dce56fc3
Benjamin ‘Touky’ Huet <huet.benjamin@gmail.com>
2014-10-26 01:22:27 +0000

    Added Assimp on base, integration is coming up

---
+++ assimp-3.1.1/COPY_LIB.bat
@@ -0,0 +1,11 @@
+copy /Y ".\workspaces\vs2013\x64\code\Release\assimp.lib" ".\lib\x64\assimp.lib"
+copy /Y ".\workspaces\vs2013\x64\code\Debug\assimpd.lib" ".\lib\x64\assimpd.lib"
+copy /Y ".\workspaces\vs2013\x64\contrib\zlib\Release\zlibstatic.lib" ".\lib\x64\zlibstatic.lib"
+copy /Y ".\workspaces\vs2013\x64\contrib\zlib\Debug\zlibstaticd.lib" ".\lib\x64\zlibstaticd.lib"
+
+copy /Y ".\workspaces\vs2013\win32\code\Release\assimp.lib" ".\lib\win32\assimp.lib"
+copy /Y ".\workspaces\vs2013\win32\code\Debug\assimpd.lib" ".\lib\win32\assimpd.lib"
+copy /Y ".\workspaces\vs2013\win32\contrib\zlib\Release\zlibstatic.lib" ".\lib\win32\zlibstatic.lib"
+copy /Y ".\workspaces\vs2013\win32\contrib\zlib\Debug\zlibstaticd.lib" ".\lib\win32\zlibstaticd.lib"
+
+pause
