84624715e1cb7358e9d96bf46e66d347f5171ae1
Sam Hocevar <sam@hocevar.net>
2020-05-19 11:17:08 +0200

    Update OpenSSL to 1.1.1g and add PDBs.

--- download-openssl-msvc.sh
+++ download-openssl-msvc.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 
+set -x
 set -e
 
 CACHE=.conan-cache
 
-BUILD=Release
+BUILD="Release"
+ARCH_LIST="i686 x86_64"
 
-## Install on MSYS2:
-# pacman -S mingw64/mingw-w64-x86_64-python3-markupsafe
-# pacman -S mingw64/mingw-w64-x86_64-python3-yaml
-# pip3 install conan
+# Packages we want
+#PKG_LIST="openssl zlib sdl2 sdl2_mixer sdl2_image"
+PKG_LIST="openssl"
 
-for arch in i686 x86_64; do
+# Packages that need building because the binary package doesn’t exist
+# or does not contain necessary information such as the PDBs.
+BUILD_LIST="openssl"
+
+for arch in $(echo $ARCH_LIST); do
     conan_arch="$(echo $arch | sed 's/i686/x86/')"
-    conan install -s build_type="${BUILD}" -s arch="${conan_arch}" -if ${CACHE} . \
-      || conan install -s build_type="${BUILD}" -s arch="${conan_arch}" --build openssl -if ${CACHE} .
-    for pkg in openssl zlib sdl2; do
+
+    conan install -s build_type="${BUILD}" -s arch="${conan_arch}" --build ${BUILD_LIST} -if ${CACHE} .
+
+    for pkg in $(echo $PKG_LIST); do
         install_path="$(awk "/<Conan-${pkg}-Root>/ { gsub(/ *<[^>]*> */, "'""'"); print }" "${CACHE}/conanbuildinfo.props")"
-        chunks="$(echo "$install_path" | sed 's@.*\.conan/data/@@')"
-        test "${pkg}" = "$(echo "$chunks" | cut -f1 -d/)"
+        source_path="$(echo "${install_path}" | sed 's@_/package/@_/build/@')"
+        chunks="$(echo "${install_path}" | sed 's@.*\.conan/data/@@')"
+        test "${pkg}" = "$(echo "${chunks}" | cut -f1 -d/)"
         version="$(echo "$chunks" | cut -f2 -d/)"
         lib_path="${pkg}-${version}/lib/${arch}-msvc"
         include_path="${pkg}-${version}/include"
+
         rm -rf "${lib_path}" "${include_path}"
         mkdir -p "${lib_path}" "${include_path}"
+        find "${source_path}/source_subfolder" -name '*static.pdb' -exec cp {} "${lib_path}/" ';'
         cp -rv "${install_path}"/lib/* "${lib_path}/"
         cp -rv "${install_path}"/include/* "${include_path}/"
     done
